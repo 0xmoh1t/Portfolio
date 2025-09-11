890e225a47d1ae67e389c08ac902a8aea0b91c87
Mohit Dad(Maheshwari) <123006118+mohitgit-hub@users.noreply.github.com>
2025-09-12 00:56:47 +0530

    Contact, footer and header background done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
+
+const scrollHeader = () => {
+    const header = document.getElementById('header');
+
+    this.scrollY >= 20 
+    ? header.classList.add('scroll-header') 
+    : header.classList.remove('scroll-header');
+};
+
+window.addEventListener('scroll', scrollHeader)
+
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.utils.toArray('.text-gradient').forEach((span) => {
     gsap.to(span, {
-    backgroundSize: '100% 100%',
-    ease: 'none',
-    scrollTrigger: {
-        trigger: span,
-        start: 'top bottom',
-        end: 'top center',
-        scrub: true,
-    },
+        backgroundSize: '100% 100%',
+        ease: 'none',
+        scrollTrigger: {
+            trigger: span,
+            start: 'top bottom',
+            end: 'top center',
+            scrub: true,
+        },
     });
 });
 // Mixitup animation
@@ -34,3 +45,48 @@ function activeWork() {
 }
 
 linkWork.forEach((a) => a.addEventListener('click', activeWork))
+
+const contactForm = document.getElementById('contact-form'),
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactMessage = document.getElementById('contact-message'), // textarea, not label
+    message = document.getElementById('message'); // <p id="message">
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (contactName.value === '' ||
+        contactEmail.value === '' ||
+        contactMessage.value === '') {
+        message.textContent = 'Write all the input fields';
+
+        setTimeout(() => {
+            message.textContent = ''
+        }, 3000)
+    }
+    else {
+        emailjs.sendForm(
+            "service_i266bxs",
+            "template_2pqhimu",
+            '#contact-form',
+            "o70Mdwzy1akDN2FiW"
+        ).then(
+            () => {
+                message.textContent = 'Message sent ✓';
+                setTimeout(() => {
+                    message.textContent = ''
+                }, 5000)
+            },
+            (error) => { alert('OOPS! SOMETHING WENT WRONG...', error); }
+        );
+
+        contactName.value = '';
+        contactEmail.value = '';
+        contactMEssage.value = '';
+
+    }
+};
+
+
+contactForm.addEventListener('submit', sendEmail);
+
