16d87b3b4d960eb575377245b4c7251742bf8427
Mohit Dad(Maheshwari) <123006118+mohitgit-hub@users.noreply.github.com>
2025-09-12 18:14:39 +0530

    Darkmode added, scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,27 @@ const scrollHeader = () => {
 
 window.addEventListener('scroll', scrollHeader)
 
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id'),
+        sectionClass = document.querySelector('.nav-menu a[href*=' + sectionId + ']');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('.active-links');
+        } else {
+            sectionClass.classList.remove('active-links');
+        }
+    });
+};
+window.addEventListener('scroll', scrollActive)
+
+
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.utils.toArray('.text-gradient').forEach((span) => {
@@ -23,6 +44,33 @@ gsap.utils.toArray('.text-gradient').forEach((span) => {
         },
     });
 });
+
+//Darkmode 
+
+window.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.getElementById('theme-toggle');
+
+    function applyTheme(theme) {
+        if(theme === 'light') {
+            document.body.classList.add('light-theme');
+            toggleBtn.classList.remove('ri-sun-line')
+            toggleBtn.classList.add('ri-moon-line')
+        }
+        else {
+            document.body.classList.remove('light-theme');
+            toggleBtn.classList.add('ri-sun-line');
+            toggleBtn.classList.remove('ri-moon-line');
+
+        }
+
+        localStorage.setItem('theme', theme);
+    }
+
+    toggleBtn.addEventListener('click', () => {
+        const isLight = document.body.classList.contains('light-theme');
+        applyTheme(isLight ? 'dark' : 'light');
+    })
+})
 // Mixitup animation
 var mixer = mixitup('.work-container', {
     selectors: {
